6b0ce80af4447a27df479b4e80efe28b64f669d1
rosario <58920070+athena272@users.noreply.github.com>
2025-11-16 03:23:18 -0300

    Update create-database.php

--- create-database.php
+++ create-database.php
@@ -1,5 +1,8 @@
 <?php
 
-$dbPath = __DIR__ . '/db/db.sqlite';
-$pdo = new PDO("sqlite:$dbPath");
-$pdo->exec('CREATE TABLE videos (id INTEGER PRIMARY KEY, url TEXT, title TEXT);');
+require_once __DIR__ . '/src/Database.php';
+
+$pdo = Database::getConnection();
+$pdo->exec('CREATE TABLE IF NOT EXISTS videos (id INTEGER PRIMARY KEY, url TEXT, title TEXT);');
+
+echo "Banco de dados criado com sucesso!\n";
